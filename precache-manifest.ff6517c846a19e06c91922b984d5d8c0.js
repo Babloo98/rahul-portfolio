dcad1bc9122ff7b872af2eea83277a7cb28b8a57
babloo <babloo.mishra@tothenew.com>
2019-07-23 07:51:30 +0530

    Updates

---
+++ precache-manifest.ff6517c846a19e06c91922b984d5d8c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c1a3e0073ccf70b03be28bb12bc5c6",
+    "url": "/rahul-portfolio/index.html"
+  },
+  {
+    "revision": "c67d6f01087cfef0aea3",
+    "url": "/rahul-portfolio/static/css/main.2d9df17d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/rahul-portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c67d6f01087cfef0aea3",
+    "url": "/rahul-portfolio/static/js/main.b9510ff2.chunk.js"
+  },
+  {
+    "revision": "034cf7444c0317691b3e",
+    "url": "/rahul-portfolio/static/js/runtime~main.0c4fa94a.js"
+  }
+]);
